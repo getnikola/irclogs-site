5d3b9a4a9f7caa8e69ac24e944f028dabf88e607
Chris Warrick <kwpolska@gmail.com>
2016-05-29 19:24:23 +0200

    Send an e-mail on rebuild error
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

--- rebuild.sh
+++ rebuild.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 cd /home/kwpolska/nikola-logs
-/home/kwpolska/virtualenvs/nikola-py3/bin/nikola build > last-rebuild.log 2> last-rebuild.log
+/home/kwpolska/virtualenvs/nikola-py3/bin/nikola build > last-rebuild.log 2>&1
+if [[ $? != 0 ]]; then
+    # error reporting
+    mailfn='last-error-email'
+    dold=$(cat $mailfn)
+    dnow=$(date "+%s")
+
+    if [[ $[$dnow - $dold] -gt 86400 ]]; then
+        echo "Nikola Logs build error."
+        echo
+        cat last-rebuild.log
+        echo -n $dnow > $mailfn
+    fi
+fi
