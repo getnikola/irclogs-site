d68dfc5799c5fec977771ab24db85a7c4ae63334
Chris Warrick <kwpolska@gmail.com>
2016-12-06 19:42:03 +0100

    Less rebuilds
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

--- conf.py
+++ conf.py
@@ -601,7 +601,7 @@ LICENSE = ""
 
 # A small copyright notice for the page footer (in HTML).
 # (translatable)
-CONTENT_FOOTER = 'Maintained by <a href="mailto:{email}">{author}</a>, powered by <a href="https://getnikola.com/" rel="nofollow">Nikola</a> and <a href="https://chriswarrick.com/kwbot/">KwBot</a>.  Rebuilt every 15 minutes.'
+CONTENT_FOOTER = 'Maintained by <a href="mailto:{email}">{author}</a>, powered by <a href="https://getnikola.com/" rel="nofollow">Nikola</a> and <a href="https://chriswarrick.com/kwbot/">KwBot</a>.  Rebuilt every hour.'
 
 # Things that will be passed to CONTENT_FOOTER.format().  This is done
 # for translatability, as dicts are not formattable.  Nikola will
